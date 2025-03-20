ec43632dfc7425f86f7ef829e033dc03d099cc60
ThermalEng <38897886+ThermalEng@users.noreply.github.com>
2025-03-20 13:13:03 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -2,3 +2,5 @@
 # Please enter the custom command below (usually used to install third-party plugins) (can be left blank)
 # git clone --depth=1 https://github.com/EOYOHOO/UA2F.git package/UA2F
 # git clone --depth=1 https://github.com/EOYOHOO/rkp-ipid.git package/rkp-ipid
+sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
