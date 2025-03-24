4957064555b20c5fbb246bf1d1fbbff001f8e6e0
ThermalEng <38897886+ThermalEng@users.noreply.github.com>
2025-03-24 15:53:24 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -2,7 +2,6 @@
 # Please enter the custom command below (usually used to install third-party plugins) (can be left blank)
 # git clone --depth=1 https://github.com/EOYOHOO/UA2F.git package/UA2F
 # git clone --depth=1 https://github.com/EOYOHOO/rkp-ipid.git package/rkp-ipid
-cd ImmortalWrt
 ./scripts/feeds update -a
 rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
 rm -rf feeds/packages/net/mosdns && cp -rf ./feeds/smpackage/mosdns ./feeds/packages/net/mosdns
